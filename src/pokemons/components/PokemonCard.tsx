2dadcecc5bad2d39dea1f6147c35fd117fa68b92
Alexis Guadarrama <alexgsisc@gmail.com>
2024-02-13 17:34:10 -0600

    refresh con fetch

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -25,7 +25,7 @@ export const PokemonCard = ({ pokemon }: PokemonCardProps) => {
           </p>
           <div className="mt-5">
             <Link
-              href={`pokemon/${id}`}
+              href={`pokemons/${name}`}
              className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
               Show more...
             </Link>
